27dafe13d98562d870d0f0b393afc410ea5eaa2a
Leonardo Moreira <moreiraleos123@gmail.com>
2024-09-23 22:01:46 -0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// $app = include __DIR__ . "/../src/App/bootstrap.php";
+
+require_once "../vendor/autoload.php";
+require_once  __DIR__ . "/../src/App/bootstrap.php";
+
+$app->run();
---
+++ src/App/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+require "../vendor/autoload.php";
+// include __DIR__ . "/../Framework/App.php";
+
+use Framework\App;
+
+$app = new App();
+return $app;
